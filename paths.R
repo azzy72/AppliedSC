911855fd91645ed3206300a185fe14cb22f8565d
JoannaRasmussen <s215092@student.dtu.dk>
2026-01-19 15:46:24 +0100

    Added path for reference database

--- paths.R
+++ paths.R
@@ -1,3 +1,4 @@
-intestie_data_path <- "/home/projects/exam_2026_22102/group2/intestine_data/"
-sample_seurat_path <- paste0(intestie_data_path, "sample_seurats/")
-EC_seurats_path <- paste0(intestie_data_path, "EC_seurats/")
+intestine_data_path <- "/home/projects/exam_2026_22102/group2/intestine_data/"
+reference_data_path <- "/home/projects/exam_2026_22102/group2/reference_data/"
+sample_seurat_path <- paste0(intestine_data_path, "sample_seurats/")
+EC_seurats_path <- paste0(intestine_data_path, "EC_seurats/")
