c955cfc8b3aa061bb3a9a1cc28adbd8282546bc8
azzy72 <s215045@student.dtu.dk>
2026-01-19 08:58:02 +0100

    scaling with paths, minor edits to 02, 03, 04

---
+++ paths.R
@@ -0,0 +1,3 @@
+intestie_data_path <- "/home/projects/exam_2026_22102/group2/intestine_data/"
+sample_seurat_path <- paste0(intestie_data_path, "sample_seurats/")
+EC_seurats_path <- paste0(intestie_data_path, "EC_seurats/")
